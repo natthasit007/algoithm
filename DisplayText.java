be476dd0bffa7fea81a9735fff4a352ab05303cd
nn <674259007@webmail.npru.ac.th>
2024-06-28 10:51:27 +0700

    first commit

---
+++ DisplayText.java
@@ -0,0 +1,7 @@
+public class DisplayText {
+    public static void main(Strring[] args){
+    System.out.println("Natthasit" + "Oudom");
+    System.out.println(10+5);
+    System.out.println("Npru" + 2567);
+    System.out.println(0+ "3" + 2);
+}
