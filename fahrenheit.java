87f78aa3b39f6537f457ebe5d4a8acf6e11facf7
natthasit007 <674259007@webmail.npru.ac.th>
2024-07-30 12:09:13 +0700

    pop007

---
+++ fahrenheit.java
@@ -0,0 +1,9 @@
+public class fahrenheit {
+    public static void main(String[] args){
+        double fahrenheit = 80;
+        double celsius = (5.0/9.0)*(fahrenheit-32);
+        System.out.println(fahrenheit);
+        System.out.println(celsius);
+    }
+    
+}
